86f6b10517b743173366d56525873e1da6430a54
Michael Kubiaczyk <48311127+cx-michael-kubiaczyk@users.noreply.github.com>
2026-06-17 10:21:00 +0200

    removing old deprecation-flagged functions

--- presets.go
+++ presets.go
@@ -506,11 +506,6 @@ func (c *Cx1Client) DeletePreset(preset Preset) error {
 	return err
 }
 
-func (c *Cx1Client) PresetLink(p *Preset) string {
-	c.depwarn("PresetLink", "will be removed")
-	return fmt.Sprintf("%v/resourceManagement/presets?presetId=%v", c.config.Cx1Url, p.PresetID)
-}
-
 func (p Preset) GetSASTQueryCollection(queries SASTQueryCollection) SASTQueryCollection {
 	coll := SASTQueryCollection{}
 	if p.Engine != "sast" {
