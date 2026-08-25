6b2f5e8fccb708fb1cdeedbcaf8a8bf216f617b8
Michael Kubiaczyk <48311127+cx-michael-kubiaczyk@users.noreply.github.com>
2026-08-25 09:41:30 +0200

    updates to old query api Levels based on cx1 version

--- audit_v310.go
+++ audit_v310.go
@@ -29,12 +29,14 @@ type AuditQuery_v310 struct {
 	Title              string
 }
 
-var AUDIT_QUERY_v310 = struct {
+type QUERY_LEVELS_v310 struct {
 	PRODUCT     string
 	TENANT      string
 	APPLICATION string
 	PROJECT     string
-}{"Cx", "Corp", "Team", "Project"}
+}
+
+var audit_query_v310 = QUERY_LEVELS_v310{"Cx", "Corp", "Team", "Project"}
 
 func (q *AuditQuery_v310) ParsePath() {
 	s := strings.Split(q.Path, "/")
@@ -87,7 +89,7 @@ func (q AuditQuery_v310) ToQuery() SASTQuery {
 		CweID:              q.Cwe,
 		IsExecutable:       q.IsExecutable,
 		QueryDescriptionId: q.CxDescriptionId,
-		Custom:             q.Level != AUDIT_QUERY_v310.PRODUCT,
+		Custom:             q.Level != audit_query_v310.PRODUCT,
 		EditorKey:          q.Key,
 		SastID:             0,
 	}
@@ -95,19 +97,23 @@ func (q AuditQuery_v310) ToQuery() SASTQuery {
 
 func (q AuditQuery_v310) CreateTenantOverride() AuditQuery_v310 {
 	new_query := q
-	new_query.Level = AUDIT_QUERY_v310.TENANT
-	new_query.LevelID = AUDIT_QUERY_v310.TENANT
+	new_query.Level = audit_query_v310.TENANT
+	new_query.LevelID = audit_query_v310.TENANT
 	return new_query
 }
 func (q AuditQuery_v310) CreateProjectOverrideByID(projectId string) AuditQuery_v310 {
 	new_query := q
-	new_query.Level = AUDIT_QUERY_v310.PROJECT
+	new_query.Level = audit_query_v310.PROJECT
 	new_query.LevelID = projectId
 	return new_query
 }
 func (q AuditQuery_v310) CreateApplicationOverrideByID(applicationId string) AuditQuery_v310 {
 	new_query := q
-	new_query.Level = AUDIT_QUERY_v310.APPLICATION
+	new_query.Level = audit_query_v310.APPLICATION
 	new_query.LevelID = applicationId
 	return new_query
+}
+
+func (c *Cx1Client) AuditQueryLevels_v310() QUERY_LEVELS_v310 {
+	return audit_query_v310
 }
